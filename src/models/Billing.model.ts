29fd8b1c3cf97fa9b9ccba93549cfff60269dbfd
Wellington Abreu <wellington.abreu@gmail.com>
2024-01-07 21:35:05 -0400

    Added stripe and incorrect feedback

---
+++ src/models/Billing.model.ts
@@ -0,0 +1,18 @@
+import { Timestamp } from "mongodb";
+import mongoose, { Schema } from "mongoose";
+
+export const billingSchema = new Schema({
+  documentType: String,
+  checkoutId: String,
+  subscriptionId: String,
+  invoice: String,
+  currency: String,
+  amountTotal: Number,
+  status: String,
+  customerEmail: String,
+  customerName: String,
+  createdAt: Date,
+  expiresAt: Date,
+});
+
+export const Billing = mongoose.model("Billing", billingSchema, "billings");
